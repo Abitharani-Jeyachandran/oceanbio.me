2e967fde103002d61f28168583fc79cd8121b972
DESKTOP-O6AH0ED\ajaya <ajayanthan97@gmail.com>
2021-02-01 23:08:52 +0530

    Revert "Add V2 webfiles"
    This reverts commit 5f3b5e8e46933dcef7cb3b4774d9ac755a02436a.

--- pages/0-0-header.php
+++ pages/0-0-header.php
@@ -1,53 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="template-page-home-1">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <!-- Primary Meta Tags -->
-  <title>OceanBiome</title>
-  <meta name="title" content="OceanBiome">
-  <meta name="description" content="OceanBiome is a community of talents with a focus on ocean research to map the path to engage in Ocean Protection to enable the next generation of leaders to engage in ocean literacy and stewardship to become ocean ambassadors in Sri Lanka">
-
-  <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://metatags.io/">
-  <meta property="og:title" content="OceanBiome">
-  <meta property="og:description" content="OceanBiome is a community of talents with a focus on ocean research to map the path to engage in Ocean Protection to enable the next generation of leaders to engage in ocean literacy and stewardship to become ocean ambassadors in Sri Lanka">
-  <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-
-  <!-- Twitter -->
-  <meta property="twitter:card" content="summary_large_image">
-  <meta property="twitter:url" content="https://metatags.io/">
-  <meta property="twitter:title" content="OceanBiome">
-  <meta property="twitter:description" content="OceanBiome is a community of talents with a focus on ocean research to map the path to engage in Ocean Protection to enable the next generation of leaders to engage in ocean literacy and stewardship to become ocean ambassadors in Sri Lanka">
-  <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-
-  <!-- Favicons -->
-  <link rel="shortcut icon" type="image/x-icon" href="../assets\img\favicon-ocean-biome\favicon.ico">
-  <link rel="apple-touch-icon" sizes="180x180" href="../assets\img\favicon-ocean-biome\apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../assets\img\favicon-ocean-biome\favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../assets\img\favicon-ocean-biome\favicon-16x16.png">
-  <link rel="manifest" href="../assets\img\favicon-ocean-biome\site.webmanifest">
-  <!-- Google Fonts -->
-  <link href="http://fonts.googleapis.com/css?family=Raleway:600,700,800" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="../assets\vendor\font-awesome/font-awesome.css" rel="stylesheet" type="text/css">
-  <?php include('0-1-cookie-consent.php'); ?>
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
+	<title>Ocean Biome</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="We are a group of young people who are captivated by oceanic research and we want to construct an environment for our next generation to involve,learn and engage in the study of the ocean." />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700italic,700,400italic%7CLato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic%7COpen+Sans:Open+Sans:400,800italic,800,700italic,700,600italic,600,400italic,300italic,300%7CHandlee">
+	<link rel="stylesheet" type="text/css" href="../style/megafish.css" />
+	<link rel="stylesheet" type="text/css" href="../style/superfish.css" />
+	<link rel="stylesheet" type="text/css" href="../style/flexslider.css" />
+	<link rel="stylesheet" type="text/css" href="../style/jquery.qtip.css" />
+	<link rel="stylesheet" type="text/css" href="../style/jquery-ui.min.css" />
+	<link rel="stylesheet" type="text/css" href="../style/jquery-supersized.css" />
+	<link rel="stylesheet" type="text/css" href="../style/jquery.nivo.slider.css" />
+	<link rel="stylesheet" type="text/css" href="../style/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" type="text/css" href="../style/fancybox/helpers/jquery.fancybox-buttons.css" />
+	<link rel="stylesheet" type="text/css" href="../style/revslider/layers.css" />
+	<link rel="stylesheet" type="text/css" href="../style/revslider/settings.css" />
+	<link rel="stylesheet" type="text/css" href="../style/revslider/navigation.css" />
+	<link rel="stylesheet" type="text/css" href="../style/base.css" />
+	<link rel="stylesheet" type="text/css" href="../style/responsive.css" />
+	<link rel="stylesheet" type="text/css" href="../style/retina.css" />
+	<script type="text/javascript" src="../script/jquery.min.js"></script>
+	<script type="text/javascript" src="../script/google-form.js"></script>
+	<?php include('0-1-cookies-consent.php'); ?>
 </head>
 
-<body>
-  <?php include('0-2-menu-bar.php'); ?>
+<body class="template-page-home-1">
+<?php include('0-2-menu-bar.php'); ?>
