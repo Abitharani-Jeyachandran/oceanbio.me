eaeefecf125996ec2f92636ad688691e2c28e701
DESKTOP-O6AH0ED\ajaya <ajayanthan97@gmail.com>
2021-02-02 15:15:12 +0530

    Add webdiles new

---
+++ pages/1-f-contact.php
@@ -0,0 +1,57 @@
+<!-- ======= Contact Us Section ======= -->
+<section id="contact" class="contact">
+  <div class="container">
+
+    <div class="section-title col-xl-8 mx-auto text-center">
+      <span data-aos="fade-up">Contact Us</span>
+      <p></p>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="100">
+        <div class="info-box text-center">
+          <i class="bx bx-map"></i>
+          <h3>Our Address</h3>
+          <p>DreamSpace Academy 7A, Saravana Road, Kallady Batticaloa, 30000 Sri Lanka.</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+        <div class="info-box text-center">
+          <i class="bx bx-envelope"></i>
+          <h3>Email Us</h3>
+          <p>info@oceanbio.me</br></br></p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+        <div class="info-box text-center">
+          <i class="bx bx-phone-call"></i>
+          <h3>Call Us</h3>
+          <p>+94 75 494 9559<br>+94 77 180 1841</p>
+        </div>
+      </div>
+    </div>
+
+    <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSel05hCMj3REjK6TUg-DksPdnmG-OSgStItK7QfIwTMZr1x-Q/formResponse" method="post" role="form" class="php-email-form mt-4" data-aos="fade-up" data-aos-delay="400">
+      <div class="form-row">
+        <div class="col-md-6 form-group">
+          <input type="text" name="entry.1220691551" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+          <div class="validate"></div>
+        </div>
+        <div class="col-md-6 form-group">
+          <input type="email" class="form-control" name="entry.978091247" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+          <div class="validate"></div>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" name="entry.1418188326" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+        <div class="validate"></div>
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" name="entry.95157140" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+        <div class="validate"></div>
+      </div>
+      <div class="text-center"><button type="submit">Send Message</button></div>
+    </form>
+
+  </div>
+</section><!-- End Contact Us Section -->
