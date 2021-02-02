dcbc9479d7cbca5572a24d14bb68f24830a5036b
DESKTOP-O6AH0ED\ajaya <ajayanthan97@gmail.com>
2021-02-02 15:12:23 +0530

    Revert "Remove files from main"
    This reverts commit 3ac43234d03b09c9ca6f26a6154e508ea717894e.

---
+++ pages/2-a-team.php
@@ -0,0 +1,80 @@
+<!-- ======= Team Section ======= -->
+<section id="team" class="team section-bg">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title col-xl-8 mx-auto text-center">
+      <span data-aos="zoom-in" data-aos-delay="150">Core Team</span>
+      <p></p>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="pic"><img src="../assets\img\about\core-team\shanjeevan.jpg" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>Walter White</h4>
+            <span>Chief Executive Officer</span>
+            <div class="social">
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="pic"><img src="../assets\img\about\core-team\aravinth.jpg" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>Walter White</h4>
+            <span>Chief Executive Officer</span>
+            <div class="social">
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+        <div class="member">
+          <div class="pic"><img src="../assets\img\about\core-team\Nush photogenic (6 of 16).JPG" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>Sarah Jhonson</h4>
+            <span>Product Manager</span>
+            <div class="social">
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+        <div class="member">
+          <div class="pic"><img src="../assets\img\about\core-team\rakesh.JPG" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>William Anderson</h4>
+            <span>CTO</span>
+            <div class="social">
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+        <div class="member">
+          <div class="pic"><img src="../assets\img\about\core-team\akshaya.jpg" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>William Anderson</h4>
+            <span>CTO</span>
+            <div class="social">
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Team Section -->
