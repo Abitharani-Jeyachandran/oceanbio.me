064986cd04c3fdfe801f82dad54c8a6685ba9137
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-02-03 14:58:41 +0530

    Change footer padding

--- pages/3-1-footer-bar.php
+++ pages/3-1-footer-bar.php
@@ -7,7 +7,7 @@
       <a target="_blank" href="https://www.linkedin.com/company/oceanbiome" class="linkedin"><i class="icofont-linkedin"></i></a>
     </div>
     <div class="copyright">
-      &copy; <?php echo date('Y'); ?> Copyright <strong><span>OceanBiome</span></strong>. All Rights Reserved.
+      &copy; <?php echo date('Y'); ?> Copyright <strong><span><a target="_blank" href="https://oceanbio.me/pages/1-0-index.php">OceanBiome</a></span></strong>. All Rights Reserved.
     </div>
     <div class="credits">
       Co-created by <a target="_blank" href="https://www.linkedin.com/in/abitharani-jeyachandran-8372b6199/">Abitharani.J</a> & <a target="_blank" href="https://www.linkedin.com/in/shanjeevan-amalanathan">Shanjeevan.A</a></br>
