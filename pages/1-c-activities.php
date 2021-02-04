9a2d85c559613d4377bc2f208c8c758cfa6dd5ae
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-02-05 00:37:04 +0530

    Update activities section

--- pages/1-c-activities.php
+++ pages/1-c-activities.php
@@ -1,82 +1,103 @@
-<!--========================= Activity Section========================= -->
-<section id="activity" class="activity">
-<div class="container" data-aos="fade-up" data-aos-delay="100">
 <div class="section-title col-xl-8 mx-auto text-center">
   <span data-aos="fade-up">Activities</span>
   <p></p>
 </div>
-<div class="row align-items-center" data-aos="fade-up" data-aos-delay="100">
-  <div class="col-lg-6 col-md-3 col-sm-12 order-lg-2">
-    <div>
-      <img class="img-fluid" src="../assets\img\oceanbiome-activities\oceanbiome-education.jpg" alt="">
-    </div>
-  </div>
-  <div class="col-lg-6 col-md-9 col-sm-12 order-lg-1">
-    <div>
-      <h2 class="display-5">Ocean Education</h2>
-      <p>Citizen Science Education Initiative to educate children, youth, fisherman,
-and the general public on Ocean Literacy through customized interactive,
-experiential learning modules</p>
-    </div>
-  </div>
+
+<!-- ======= activity Section ======= -->
+<section id="activity" class="activity">
+  <div class="container" data-aos="fade-up">
+
+<div id="background-wrap">
+<div class="bubble x1"></div>
+<div class="bubble x2"></div>
+<div class="bubble x3"></div>
+<div class="bubble x4"></div>
+<div class="bubble x5"></div>
+<div class="bubble x6"></div>
+<div class="bubble x7"></div>
+<div class="bubble x8"></div>
+<div class="bubble x9"></div>
+<div class="bubble x10"></div>
+<div class="bubble x11"></div>
+<div class="bubble x12"></div>
+<div class="bubble x13"></div>
+<div class="bubble x14"></div>
+<div class="bubble x15"></div>
+<div class="bubble x16"></div>
+<div class="bubble x17"></div>
+<div class="bubble x18"></div>
+<div class="bubble x19"></div>
+<div class="bubble x20"></div>
 </div>
-</br>
-<div class="row align-items-center" data-aos="fade-up">
-  <div class="col-lg-6 col-md-3 col-sm-12">
-    <div>
-      <img class="img-fluid" src="../assets\img\oceanbiome-activities\oceanbiome-activities-story-telling.jpg" alt="">
+
+    <div class="row">
+      <div class="col-lg-6 mx-auto col-md-8 col-sm-8 order-1 order-lg-1" data-aos="fade-up" data-aos-delay="400">
+        <div class="activity-img">
+          <img src="../assets\img\oceanbiome-activities\oceanbiome-education.jpg" alt="">
+        </div>
+      </div>
+      <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-up" data-aos-delay="600">
+        <h3 class="col-lg-6 mx-auto">Ocean Education</h3>
+        <p class="col-lg-8 mx-auto">
+          Citizen Science Education Initiative to educate children, youth, fisherman,
+and the general public on Ocean Literacy through customized interactive,
+experiential learning modules.
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="col-lg-6 col-md-9 col-sm-12">
-    <div>
-      <h2 class="display-5">Ocean Storytelling</h2>
-      <p>Storytelling and Visual Literacy are tools to make anyone easily
+
+  <div class="row">
+    <div class="col-lg-6 mx-auto col-md-8 col-sm-8 order-1 order-lg-1" data-aos="fade-up" data-aos-delay="400">
+      <div class="activity-img">
+        <img src="../assets\img\oceanbiome-activities\oceanbiome-activities-story-telling.jpg" alt="">
+      </div>
+    </div>
+    <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-up" data-aos-delay="600">
+      <h3 class="col-lg-6 mx-auto">Ocean Storytelling</h3>
+      <p class="col-lg-8 mx-auto">
+        Storytelling and Visual Literacy are tools to make anyone easily
 understand anything. Through Ocean Storytelling the facts of the human
 acts that unknowingly disrupt the Ocean Ecosystem will be driven as
 Visual Storytelling to make the people embody the acts that cause the
-destruction</p>
+destruction.
+      </p>
     </div>
   </div>
-</div>
-</br>
-<div class="row align-items-center" data-aos="fade-up">
-<div class="col-lg-6 col-md-3 col-sm-12 order-lg-2">
-  <div>
-    <img class="img-fluid" src="../assets\img\oceanbiome-activities\oceanbiome-activities-tech.jpg" alt="">
-  </div>
-</div>
-<div class="col-lg-6 col-md-9 col-sm-12 order-lg-1">
-  <div>
-    <h2 class="display-5">Ocean Tech</h2>
-    <p>Locally producing technology based solutions to protect the Ocean
+
+  <div class="row">
+    <div class="col-lg-6 mx-auto col-md-8 col-sm-8 order-1 order-lg-1" data-aos="fade-up" data-aos-delay="400">
+      <div class="activity-img">
+        <img src="../assets\img\oceanbiome-activities\oceanbiome-activities-tech.jpg" alt="">
+      </div>
+    </div>
+    <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-up" data-aos-delay="600">
+      <h3 class="col-lg-6 mx-auto">Ocean Tech</h3>
+      <p class="col-lg-8 mx-auto">
+        Locally producing technology based solutions to protect the Ocean
 Ecosystem. Identifying the local talents passionate about the ocean, and
 marine lives to engage them in challenge-based workshops to come with
-innovative solutions for ocean problems</p>
-  </div>
-</div>
-</div>
-</br>
-<div class="row align-items-center" data-aos="fade-up">
-  <div class="col-lg-6 col-md-3 col-sm-12">
-    <div>
-      <img class="img-fluid" src="../assets\img\oceanbiome-activities\oceanbiome-activities-ocean-community.jpg" alt="">
+innovative solutions for ocean problems.
+      </p>
     </div>
   </div>
-  <div class="col-lg-6 col-md-9 col-sm-12">
-    <div>
-      <h2 class="display-5">Ocean Community</h2>
-      <p>Ocean community brings together diverse Ocean Advocates with
+
+  <div class="row">
+    <div class="col-lg-6 mx-auto col-md-8 col-sm-8 order-1 order-lg-1" data-aos="fade-up" data-aos-delay="400">
+      <div class="activity-img">
+        <img src="../assets\img\oceanbiome-activities\oceanbiome-activities-ocean-community.jpg" alt="">
+      </div>
+    </div>
+    <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-up" data-aos-delay="600">
+      <h3 class="col-lg-6 mx-auto">Ocean Community</h3>
+      <p class="para4 col-lg-8 mx-auto">
+        Ocean community brings together diverse Ocean Advocates with
 multidisciplinary backgrounds and establishes a network to connect local
 and international Ocean-related experts, scientists, and organizations to
-nurture local individuals in Sri Lanka to protect the Ocean Ecosystem</p>
+nurture local individuals in Sri Lanka to protect the Ocean Ecosystem.
+      </p>
     </div>
   </div>
-</div>
 
-</div>
-</section>
 
-<section id="about-boxes" class="about-boxes2">
-<div class="container" data-aos="fade-up">
-</div>
-</section>
+  </div>
+</section><!-- End activity Section -->
