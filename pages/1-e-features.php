11a852d2c7f26830084ef0c3ceec4525d8779ef1
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-10-05 20:41:01 +0530

    Add timeline

--- pages/1-e-features.php
+++ pages/1-e-features.php
@@ -63,3 +63,5 @@
 <div class="container" data-aos="fade-up">
 </div>
 </section>
+
+<iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1flN9xi8yJihGSBuByd5P6l78FVnps1P9aiVLshuO7YA&font=Default&lang=en&start_at_end=true&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
