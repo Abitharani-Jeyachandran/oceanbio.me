fe406ff7593862d80fcfa407ab8682e1272e87cf
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-11-14 20:15:29 +0530

    Add tutorial videos

--- pages/1-f-tutorials.php
+++ pages/1-f-tutorials.php
@@ -10,17 +10,29 @@
   <div class="container">
 
     <div class="row">
-      <div class="col-md-6 d-flex align-items-stretch">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0tYGQCDf_-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="col-md-3 d-flex align-items-stretch">
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/0tYGQCDf_-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/BFqZWPsUE4c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="col-md-3 d-flex align-items-stretch">
+<iframe width="280" height="157" src="https://www.youtube.com/embed/BFqZWPsUE4c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class="col-md-6 d-flex align-items-stretch mt-4">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/ywMjGBxRY8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="col-md-3 d-flex align-items-stretch">
+<iframe width="280" height="157" src="https://www.youtube.com/embed/ywMjGBxRY8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class="col-md-6 d-flex align-items-stretch mt-4">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/8qRdIYeW9a4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="col-md-3 d-flex align-items-stretch">
+<iframe width="280" height="157" src="https://www.youtube.com/embed/8qRdIYeW9a4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+
+    <div class="row pt-4">
+      <div class="col-md-3 d-flex align-items-stretch">
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/VJOXvXPu1cM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="col-md-3 d-flex align-items-stretch">
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/HZIcy5e2zqA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="col-md-3 d-flex align-items-stretch">
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/-Nf5y_4CFho" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
 
