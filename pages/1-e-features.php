c8a31d39bea284d45d6abe22e6927e592f6181d3
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-11-14 20:18:43 +0530

    Comment timeline

--- pages/1-e-features.php
+++ pages/1-e-features.php
@@ -3,6 +3,6 @@
 <div class="container" data-aos="fade-up">
 </div>
 </section>
-<div class="container pt-4">
+<!--<div class="container pt-4">
 <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1flN9xi8yJihGSBuByd5P6l78FVnps1P9aiVLshuO7YA&font=Default&lang=en&start_at_end=true&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
-</div>
+</div>-->
