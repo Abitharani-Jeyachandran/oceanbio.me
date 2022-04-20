7ef9dd1b040ad68f35fba69c764e6f73e03bfe75
Abitharani-Jeyachandran <abitharani009@gmail.com>
2022-04-20 23:14:16 +0530

    Add file path

--- pages/call-for-position.php
+++ pages/call-for-position.php
@@ -1,3 +1,3 @@
 <div class="template-content">
-<iframe src="Position_ Field Officer.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
+<iframe src="../assets/Position_ Field Officer.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
 </div>
