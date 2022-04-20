eab8a5b6c151a15150c8ca32e6a86ca87b4219ff
Abitharani-Jeyachandran <abitharani009@gmail.com>
2022-04-20 19:48:27 +0530

    Change pdf

--- pages/call-for-position.php
+++ pages/call-for-position.php
@@ -1,3 +1,3 @@
 <div class="template-content">
-<iframe src="../assets/position_ field_officer.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
+<iframe src="../assets/position_field_officer.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
 </div>
