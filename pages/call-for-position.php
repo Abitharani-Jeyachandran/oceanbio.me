f766f21357198906de2d77bf13634531d229155c
Abitharani-Jeyachandran <abitharani009@gmail.com>
2022-04-20 18:09:14 +0530

    Change name

--- pages/call-for-position.php
+++ pages/call-for-position.php
@@ -1,5 +1,5 @@
 <?php include('0-0-header.php'); ?>
 <div class="template-content">
-<iframe src="../assets/position_field_officer.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
+<iframe src="../assets/position_field_officer.pdf" type="application/pdf" height="750vh" width="100%" class="responsive"></iframe>
 </div>
 <?php include('9-0-footer.php'); ?>
